fbb605a1131103977ec9ce46b05f89f746d8b0da
Gabriel Oliveira <oliveiragabriel1712@gmail.com>
2023-11-25 13:14:29 -0300

    Feat: Add um novo mapa e geolocalizacao

---
+++ src/Mapa/script.js
@@ -0,0 +1,32 @@
+
+let h2 = document.querySelector('h2');
+var map;
+
+function success(pos) {
+    console.log(pos.coords.latitude, pos.coords.longitude);
+    // h2.textContent = `Latitude:${pos.coords.latitude}, Longitude:${pos.coords.longitude}`;
+
+    if (map === undefined) {
+        map = L.map('mapid').setView([pos.coords.latitude, pos.coords.longitude], 14);
+    } else {
+        map.remove();
+        map = L.map('mapid').setView([pos.coords.latitude, pos.coords.longitude], 13);
+    }
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        // attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([pos.coords.latitude, pos.coords.longitude]).addTo(map)
+        .bindPopup('Eu estou aqui!')
+        .openPopup();
+}
+
+function error(err) {
+    console.log(err);
+}
+
+var watchID = navigator.geolocation.watchPosition(success, error, {
+    enableHighAccuracy: true,
+    timeout: 5000
+});
