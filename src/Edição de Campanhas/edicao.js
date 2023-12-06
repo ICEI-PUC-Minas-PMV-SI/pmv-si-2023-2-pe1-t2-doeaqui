c542a264a56a9fa2df81f37e337f463bc433aaa1
guigsmendonca <guigsguilherme.gds@gmail.com>
2023-12-06 11:07:31 -0300

    Create edicao.js

---
+++ src/Edição de Campanhas/edicao.js
@@ -0,0 +1,13 @@
+const formCampanha= document.querySelector("formCampanha");
+formCampanha.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const nomeCampanha = document.querySelector("nomeCampanha") ;
+    const causasApoiadas = document.querySelector("causasApoiadas") ;
+    const vigencia = document.querySelector("vigencia") ;
+    const itensArrecadados = document.querySelector("itensArrecadados") ;
+    localStorage.setItem("nomeCampanha", nomeCampanha.value);
+    localStorage.setItem("causasApoiadas", causasApoiadas.value);
+    localStorage.setItem("vigencia",vigencia.value);
+    localStorage.setItem("itensArrecadados",itensArrecadados.value);
+
+});
