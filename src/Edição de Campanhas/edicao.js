be711286c79de1e8265f2b73416d1db9ab824387
guigsmendonca <guigsguilherme.gds@gmail.com>
2023-12-07 12:08:54 -0300

    insert-ID-on-campaign
    Inserção do ID para as campanhas

--- src/Edição de Campanhas/edicao.js
+++ src/Edição de Campanhas/edicao.js
@@ -1,24 +1,46 @@
-
 document.addEventListener("DOMContentLoaded", function() {
     const editForm = document.querySelector("#editForm");
-    const dadosCampanha = JSON.parse(localStorage.getItem("dadosCampanha"));
-    document.querySelector("#nomeCampanha").value = dadosCampanha.nomeCampanha;
-    document.querySelector("#causasApoiadas").value = dadosCampanha.causasApoiadas;
-    document.querySelector("#vigencia").value = dadosCampanha.vigencia;
-    document.querySelector("#itensArrecadados").value = dadosCampanha.itensArrecadados;
+    const selectCampaign = document.querySelector("#selectCampaign");
+    const editButton = document.querySelector("#editButton");
 
-    editForm.addEventListener("submit", function(e) {
-        e.preventDefault();
-    
-        dadosCampanha.nomeCampanha = document.querySelector("#nomeCampanha").value;
-        dadosCampanha.causasApoiadas = document.querySelector("#causasApoiadas").value;
-        dadosCampanha.vigencia = document.querySelector("#vigencia").value;
-        dadosCampanha.itensArrecadados = document.querySelector("#itensArrecadados").value;
-    
-        localStorage.setItem("dadosCampanha", JSON.stringify(dadosCampanha));
-        alert("Alterações salvas com sucesso!");
-        
-        console.log("Dados alterados:", dadosCampanha);
+    editButton.addEventListener("click", function() {
+        const selectedCampaignID = document.querySelector("#campanhas").value;
+
+        if (!selectedCampaignID) {
+            alert("Não existem campanhas criadas para edição.");
+            return;
+        }
+
+        const selectedCampaignData = JSON.parse(localStorage.getItem(selectedCampaignID));
+
+        if (!selectedCampaignData) {
+            alert("Campanha selecionada não encontrada.");
+            return;
+        }
+
+        document.querySelector("#nomeCampanha").value = selectedCampaignData.nomeCampanha;
+        document.querySelector("#causasApoiadas").value = selectedCampaignData.causasApoiadas;
+        document.querySelector("#vigencia").value = selectedCampaignData.vigencia;
+        document.querySelector("#itensArrecadados").value = selectedCampaignData.itensArrecadados;
+
+
+        selectCampaign.style.display = "none";
+        editForm.style.display = "block";
     });
-});
 
+    const campanhas = JSON.parse(localStorage.getItem("campanhas")) || [];
+    const campanhasDropdown = document.querySelector("#campanhas");
+
+    if (campanhas.length === 0) {
+        editButton.disabled = true;
+        editButton.textContent = "Não há campanhas para editar";
+    } else {
+        campanhas.forEach(function(campanhaID) {
+            const campanhaData = JSON.parse(localStorage.getItem(campanhaID));
+            const option = document.createElement("option");
+            option.value = campanhaID;
+            option.textContent = campanhaData.nomeCampanha;
+            campanhasDropdown.appendChild(option);
+        });
+    }
+});
