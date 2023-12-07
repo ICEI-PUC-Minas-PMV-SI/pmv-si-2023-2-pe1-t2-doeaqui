49aeb569a20ace49569e6ba027f1965ac630957f
guigsmendonca <guigsguilherme.gds@gmail.com>
2023-12-07 16:16:56 -0300

    add-logs
    Adição de logs nos eventos

--- src/wireframe 8/deletecampaign.js
+++ src/wireframe 8/deletecampaign.js
@@ -1,7 +1,7 @@
-
 function getStoredCampanhas() {
     const campanhas = JSON.parse(localStorage.getItem('campanhas'));
-    return campanhas || []; 
+    console.log('Campanhas recuperadas:', campanhas); 
+    return campanhas || [];
 }
 
 function populateCampaignDropdown() {
@@ -12,35 +12,39 @@ function populateCampaignDropdown() {
 
     campanhas.forEach((campanha, campanhaID) => {
         const option = document.createElement('option');
-        option.value = campanhaID; 
+        option.value = campanhaID;
         option.textContent = campanha.nomeCampanha;
         dropdown.appendChild(option);
     });
+
+    console.log('Dropdown populado com campanhas:', campanhas); 
 }
 
-
 document.getElementById('deleteButton').addEventListener('click', function() {
+    console.log('Botão de exclusão clicado.'); 
     populateCampaignDropdown();
 });
 
-
 document.getElementById('confirmDelete').addEventListener('click', function() {
+    console.log('Botão de confirmação de exclusão clicado.'); 
     const selectedCampaignId = document.getElementById('campaignDropdown').value;
- 
+    const campanhas = getStoredCampanhas();
+
     if (selectedCampaignId !== '') {
         localStorage.removeItem(selectedCampaignId);
- 
+
         const campaigns = JSON.parse(localStorage.getItem('campanhas')) || [];
         const campaignIndex = campaigns.indexOf(selectedCampaignId);
- 
+
         if (campaignIndex !== -1) {
             campaigns.splice(campaignIndex, 1);
             localStorage.setItem('campanhas', JSON.stringify(campaigns));
+            alert('Campanha excluída com sucesso!');
+            window.location.reload();
+        } else {
+            alert('A campanha não foi encontrada no registro de campanhas.');
         }
- 
-        alert('Campanha excluída com sucesso!');
-        window.location.reload();
     } else {
         alert('Por favor, selecione uma campanha para excluir.');
     }
- });
+});
