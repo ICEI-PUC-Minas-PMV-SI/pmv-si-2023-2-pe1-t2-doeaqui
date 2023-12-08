6ac02d886c4ca0699c8b0c2e544565ef70694299
CaioMMelo <caio.v.mangueira@gmail.com>
2023-12-08 10:52:54 -0300

    Login atualizado

--- src/login/functions1.js
+++ src/login/functions1.js
@@ -1,18 +1,25 @@
 
+const Formlogin = document.querySelector("#Formlogin")
+Formlogin.addEventListener("submit", 
+(e) => { 
+    e.preventDefault()
+const Email = document.querySelector("#Email");
+const password = document.querySelector("#password");
 
-    const entrar = document.querySelector("#entrar");
-    entrar.addEventListener("click", logar);
+let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+let usuario = usuarios.find(u => u.email=== Email.value); 
+console.log(usuario);
+if (usuario.password === password.value) {
+    alert("funfou e tudo igual");
+    } else {
+    alert("não encontrado");
+    };
+
+logar()
+});
 
 
 function logar() {
 
     window.location.href ="../wireframe 8/index.html"; 
-}
-
-
-
-
-
-
-
-
+};
