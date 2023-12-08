e2f1272318f16e830e58224567f8632018a2e35e
CaioMMelo <caio.v.mangueira@gmail.com>
2023-12-08 10:45:31 -0300

    Cadastro considerando Multiplas ONGS

--- src/Cadastro/functions1.js
+++ src/Cadastro/functions1.js
@@ -4,16 +4,17 @@
     const Formcadastro= document.querySelector("#Formcadastro");
     Formcadastro.addEventListener("submit", (e) => {
         e.preventDefault();
-        const nomeONG =  document.querySelector("#nomeONG") ;
-        const Razao = document.querySelector ("#Razao"); 
+        const nomeONG =  document.querySelector("#nomeONG");
         const CNPJ = document.querySelector("#CNPJ");
         const password = document.querySelector("#password");
         const confirm_password = document.querySelector("#confirm_password");
+       
     localStorage.setItem("nomeONG", nomeONG.value);
-    localStorage.setItem("Razao", Razao.value);
+    
     localStorage.setItem("CNPJ",CNPJ.value);
     localStorage.setItem("password",password.value);
     localStorage.setItem("confirm_password",confirm_password.value);
+    
 
     proximo(password.value, confirm_password.value);
     });
@@ -21,7 +22,7 @@
 function proximo(password, confirm_password) {
     { 
         if (password=== confirm_password) { // Senhas correspondem, nenhum feedback é exibido.
-            alert( 'senhas  conferem');
+            
             window.location.href ="../Cadastro/FormEndereco.html"; 
             return true; 
         }
--- src/Cadastro/functions3.js
+++ src/Cadastro/functions3.js
@@ -1,18 +1,51 @@
 const Formcadastro3=document.querySelector("#Formcadastro3");
 Formcadastro3.addEventListener("submit",  (e) => {
     e.preventDefault(); 
-const email =document.querySelector("#email") ;
-const Telefone =document.querySelector("#Telefone") ;
-const Facebook =document.querySelector("#Facebook") ;
-const Instagram =document.querySelector("#Instagram") ;
-const outros =document.querySelector("#outros") ;
+const nomeONG = localStorage.getItem("nomeONG");
+const cnpj = localStorage.getItem("CNPJ");
+const password = localStorage.getItem("password");
+const confirm_password = localStorage.getItem("confirm_password");
+const cep = localStorage.getItem("CEP");
+const logradouro = localStorage.getItem("Logradouro");
+const Complemento = localStorage.getItem("Complemento");
+const bairro = localStorage.getItem("Bairro");
+const cidade = localStorage.getItem("Cidade");
+const estado = localStorage.getItem("estado");
+localStorage.setItem( "email", document.querySelector("#email").value)
+const email = localStorage.getItem("email");
+localStorage.setItem( "Telefone", document.querySelector("#Telefone").value)
+const Telefone = localStorage.getItem("Telefone");
+localStorage.setItem( "Facebook", document.querySelector("#Facebook").value)
+const Facebook = localStorage.getItem("Facebook");
+localStorage.setItem( "Instagram", document.querySelector("#Instagram").value)
+const Instagram = localStorage.getItem("Instagram");
+localStorage.setItem( "outros", document.querySelector("#outros").value)
+const outros = localStorage.getItem("outros");
 
-localStorage.setItem("email", email.value);
-localStorage.setItem("Telefone", Telefone.value);
-localStorage.setItem("Facebook", Facebook.value);
-localStorage.setItem("Instagram", Instagram.value);
-localStorage.setItem("outros", outros.value);
+    let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
 
+        const usuario = {
+            id: usuarios.length + 1, // ID único baseado na quantidade de usuários cadastrados
+nomeONG,
+cnpj,
+password,
+confirm_password,
+cep,
+logradouro,
+Complemento,
+bairro,
+cidade,
+estado,
+email,
+Telefone,
+Facebook,
+Instagram,
+outros,
+        };
+
+localStorage.clear();
+        usuarios.push(usuario);
+        localStorage.setItem("usuarios", JSON.stringify(usuarios));
 proximo();
 
 });
