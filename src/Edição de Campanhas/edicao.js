717647f6913d5822283a26205754caf4901f2426
CaioMMelo <caio.v.mangueira@gmail.com>
2023-12-10 16:01:23 -0300

    Salvando anterações

--- src/Edição de Campanhas/edicao.js
+++ src/Edição de Campanhas/edicao.js
@@ -1,8 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
+
     const editForm = document.querySelector("#editForm");
     const queryParams = new URLSearchParams(window.location.search);
     const selectedCampaignID = queryParams.get("ID");
-    const selectedCampaignData = JSON.parse(localStorage.getItem(selectedCampaignID));
+    let campanhas = JSON.parse(localStorage.getItem("campanhas")) || [];
+    let selectedCampaignData
+
+    campanhas.find(function(c){
+        if( c.nomeCampanha==selectedCampaignID){ 
+        
+        selectedCampaignData = c ;
+
+     }}
+    );
 
     document.querySelector("#nomeCampanha").value = selectedCampaignData.nomeCampanha;
     document.querySelector("#causasApoiadas").value = selectedCampaignData.causasApoiadas;
@@ -16,8 +25,13 @@ document.addEventListener("DOMContentLoaded", function() {
         selectedCampaignData.causasApoiadas = document.querySelector("#causasApoiadas").value;
         selectedCampaignData.vigencia = document.querySelector("#vigencia").value;
         selectedCampaignData.itensArrecadados = document.querySelector("#itensArrecadados").value;
-
-        localStorage.setItem(selectedCampaignID, JSON.stringify(selectedCampaignData));
-        alert("Alterações salvas com sucesso!");
+        campanhas.find(function(newc){
+            if(selectedCampaignData.idcampanha == newc.idcampanha ){
+                newc=selectedCampaignData;
+             }
+        });
+        localStorage.removeItem(campanhas)
+        localStorage.setItem("campanhas", JSON.stringify(campanhas));
+                alert("Alterações salvas com sucesso!");
     });
-});
+
